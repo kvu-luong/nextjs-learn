82ec3d84c1af67dd0b40bd603641604170f8abce
kvu-luong <luongkhanhvu1392@gmail.com>
2022-03-24 17:26:46 +0700

    get data with grapql and tailwindcss

---
+++ components/Layout.jsx
@@ -0,0 +1,13 @@
+import React, { Children } from 'react'
+import { Header } from '../components';
+
+const Layout = ({ children }) => {
+    return (
+        <>
+            <Header />
+            { children }
+        </>
+    )
+}
+
+export default Layout
---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from '../components'
+import { getPosts } from '../services'
+
+export default function Home({ posts }) {
+	// const posts = [
+	// 	{ title: 'React Testing', excerpt: 'Learn React Testing' },
+	// 	{ title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' },
+	// ]
+
+	return (
+		<div className="container mx-auto mb-8 px-10">
+			<Head>
+				<title>GraphCMS Blog</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+				{posts && posts.map((post, index) => <PostCard post={post.node} key={index} />)}
+			</div>
+			<div className="col-span-1 lg:col-span-4">
+				<div className="relative top-8 lg:sticky">
+					<PostWidget />
+					<Categories />
+				</div>
+			</div>
+		</div>
+	)
+}
+export async function getStaticProps() {
+	const posts = (await getPosts()) || []
+	return {
+		props: { posts },
+	}
+}
